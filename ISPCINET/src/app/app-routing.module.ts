8a3e72337a05858a5f8f9d529c5f979d007bba1d
Anita <anahinicoli@gmail.com>
2023-10-16 01:16:59 -0300

    Cambios leves

--- ISPCINET/src/app/app-routing.module.ts
+++ ISPCINET/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './Pages/dashboard/dashboard.component';
 import { LoginComponent } from './Auth/login/login.component';
+import { RegistroComponent } from './Auth/registro/registro.component';
 
 const routes: Routes = [ 
   {path: '', redirectTo:'/inicio', pathMatch:'full'},
   {path: 'inicio', component: DashboardComponent},
-  {path: 'iniciar-sesion', component: LoginComponent}
+  {path: 'iniciar-sesion', component: LoginComponent},
+  { path: 'registro', component: RegistroComponent}
 ];
 
 @NgModule({
